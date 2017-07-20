63b5115d0e45860c35fe6e6e7abf70a81f8d9326
Michael Hladky <michael@hladky.at>
2017-07-20 22:32:44 +0200

    added more initial state

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
   ],
+  exports: [],
   imports: [
-    BrowserModule
+    CommonModule,
+    BrowserModule,
   ],
-  providers: [],
+  providers: [  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
