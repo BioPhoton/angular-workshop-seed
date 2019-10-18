91e618b257819e591557691bb7ef3fb2f9151ede
Michael Hladky <michael@hladky.at>
2019-10-18 11:28:09 +0200

    update

--- projects/flight-api/src/lib/fake.data.ts
+++ projects/flight-api/src/lib/fake.data.ts
@@ -2,27 +2,27 @@ import {Flight} from "flight-api/src/lib/models/flight";
 
 export const flightData: Flight[] = [
   {
-    id: 1,
+    id: '1',
     from: 'Wien',
     to: 'Berlin'
   },
   {
-    id: 2,
+    id: '2',
     from: 'Wien',
     to: 'Berlin2'
   },
   {
-    id: 3,
+    id: '3',
     from: 'Wien',
     to: 'Berlin3'
   },
   {
-    id: 4,
+    id: '4',
     from: 'Wien',
     to: 'Berlin4'
   }
   ,  {
-    id: 5,
+    id: '5',
     from: 'Wien',
     to: 'Berlin5'
   }
