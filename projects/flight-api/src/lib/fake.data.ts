8680601a9ec572d6ca11d6afed62b7cfa8b68a93
Michael Hladky <michael@hladky.at>
2019-10-18 11:30:38 +0200

    update

--- projects/flight-api/src/lib/fake.data.ts
+++ projects/flight-api/src/lib/fake.data.ts
@@ -4,26 +4,31 @@ export const flightData: Flight[] = [
   {
     id: '1',
     from: 'Wien',
-    to: 'Berlin'
+    to: 'Berlin',
+    date: new Date().toISOString()
   },
   {
     id: '2',
     from: 'Wien',
-    to: 'Berlin2'
+    to: 'Berlin2',
+    date: new Date().toISOString()
   },
   {
     id: '3',
     from: 'Wien',
-    to: 'Berlin3'
+    to: 'Berlin3',
+    date: new Date().toISOString()
   },
   {
     id: '4',
     from: 'Wien',
-    to: 'Berlin4'
+    to: 'Berlin4',
+    date: new Date().toISOString()
   }
   ,  {
     id: '5',
     from: 'Wien',
-    to: 'Berlin5'
+    to: 'Berlin5',
+    date: new Date().toISOString()
   }
 ];
